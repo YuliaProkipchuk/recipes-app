622c8ed9209504cb1daa6066a9c33db5a16240ec
YuliaProkipchuk <96261460+YuliaProkipchuk@users.noreply.github.com>
2024-10-24 17:42:48 +0300

    Update store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -4,7 +4,7 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 export const store = configureStore({
     reducer: {saved:savedSlice},
   })
-  console.log('Initial state:', store.getState());
+
   export type RootState = ReturnType<typeof store.getState>;
   export const useAppDispatch: () => typeof store.dispatch = useDispatch;
   export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
